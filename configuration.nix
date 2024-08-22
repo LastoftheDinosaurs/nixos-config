75e09f097dc6a314ae17400c06fd42d37468291f
Last <last@dino.sh>
2024-08-22 00:53:58 -0500

    Add sudo rules

--- configuration.nix
+++ configuration.nix
@@ -131,6 +131,31 @@
     polkit.enable = true;
     audit.enable = true;
     auditd.enable = true;
+    sudo = {
+      enable = true;
+      extraRules = [{
+        commands = [
+          {
+            command = "${pkgs.systemd}/bin/systemctl suspend";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "${pkgs.systemd}/bin/reboot";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "${pkgs.systemd}/bin/poweroff";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+        groups = [ "wheel" ];
+      }];
+      extraConfig = with pkgs; ''
+        Defaults:picloud secure_path="${lib.makeBinPath [
+          systemd
+        ]}:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+      '';
+    };
   };
 
   # Enable experimental features in Nix
