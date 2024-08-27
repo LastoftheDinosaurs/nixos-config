fb9f925e64f0ab4aa751fd036d47c4d013f78a6b
LastoftheDinosaurs <last@dinosh>
2024-08-27 02:56:24 -0500

    Add vscode extension

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -130,6 +130,7 @@
         package = pkgs.vscodium;
         extensions = with pkgs.vscode-extensions; [
           catppuccin.catppuccin-vsc
+          catppuccin.catppuccin-vsc-icons
         ];
       };
     };
